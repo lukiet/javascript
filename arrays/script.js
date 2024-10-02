113267017a65005ab4d237c785901993fd1e26ef
Luke Tsalwa <luketsalwa@gmail.com>
2024-10-02 12:55:52 +0300

    learning arrays

---
+++ arrays/script.js
@@ -0,0 +1,11 @@
+// modifying and deleting arrays
+
+var a = ["Luke", 58, "Junior"];
+document.write(a + "<br>")
+
+a[0] = "Tsalwa"
+document.write(a + "<br>")
+
+delete a[1]
+
+document.write(a + "<br>")

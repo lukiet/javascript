bf7dc0b104495609aa7437c769dce2dc558ab1f7
Luke Tsalwa <luketsalwa@gmail.com>
2024-10-02 13:06:40 +0300

    arrays pop/push

--- arrays/script.js
+++ arrays/script.js
@@ -12,9 +12,18 @@
 
 // Array methods sort
 
+// var a = ["Nairobi", "Kitale", "Lodwar"]
+// document.write(a + "<br>")
+// a.sort()
+// document.write(a + "<br>")
+// a.reverse()
+// document.write(a + "<br>")
+
+// Array methods pop & push
+
 var a = ["Nairobi", "Kitale", "Lodwar"]
 document.write(a + "<br>")
-a.sort()
+a.pop()
 document.write(a + "<br>")
-a.reverse()
+a.push("Mombasa", "Kisumu")
 document.write(a + "<br>")

5019f83006593990f303b58bdcc26f4b348f73de
lukiet <luketsalwa@gmail.com>
2026-01-30 23:38:17 +0300

    dom

--- modules/script.js
+++ modules/script.js
@@ -159,10 +159,12 @@
 
 // Date Methods
 
-var date = new Date();
-console.log(date);
-console.log(date.getDate());
-console.log(date.getFullYear());
-console.log(date.getDay());
-console.log(date.getHours());
+// var date = new Date();
+// console.log(date);
+// console.log(date.getDate());
+// console.log(date.getFullYear());
+// console.log(date.getDay());
+// console.log(date.getHours());
+
+//DOM
 
